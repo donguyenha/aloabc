ca37f4eff336fdcd3d3a2aa855b1f8573a8ff7d5
Do Nguyen Ha <donguyenha@gmail.com>
2016-06-18 14:38:04 +0700

    update successfully directors table

--- scrapy_spider/phim_media/phim_media_get_director.py
+++ scrapy_spider/phim_media/phim_media_get_director.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import MySQLdb
 
-def InsertLink(name, cursor, connect):
-    sql = "insert into directors values('%s')" % name
+def InsertLink(name, connect, cursor):
+    sql = "insert into directors(name) values('%s')" % name
     print sql
     try:
         cursor.execute(sql)
@@ -14,17 +14,17 @@ def InsertLink(name, cursor, connect):
         return False
 
 # define connection for DB
-connect = MySQLdb.connect('localhost', 'root', 'abc@123', 'aloabc');
+connect = MySQLdb.connect('localhost', 'root', 'abc@123', 'aloabc', use_unicode=True, charset="utf8");
 cursor = connect.cursor()
 
-sql = 'select description from films where status=1'
+sql = 'select description, id from films where status=1'
 cursor.execute(sql)
 results = cursor.fetchall()
 
 for i in results:
     for j in i[0].split('\n'):
         if u'Đạo diễn' in j:
-            for x in j.split(','):
-                InsertLink(x.title(), connect, cursor)
-                #print j.title()
-                #print j.lower()
+            #print j
+            for director in j.split(':')[-1].strip().split(','):
+                # director.title() is camel case text
+                InsertLink(director.title(), connect, cursor)
